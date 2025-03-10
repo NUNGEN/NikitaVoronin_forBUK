22259fa91c0a6925b05e5c207555e011f5146f21
NUNGEN <nikita.voronin0211@gmail.com>
2025-03-10 16:34:00 +0200

    Add files via upload

---
+++ log_parser.py
@@ -0,0 +1,32 @@
+# def minus(a,b):
+#     return a-b
+# def mySymm(a,b,func):
+# #    result= a +b
+# #   return result
+#     result = func(a,b)
+#     return result
+# result1 = mySymm(5,5)
+# print(result1)
+# result2 = mySymm
+# print(result2)
+# result3 = minus
+# print(result(1,2)re)
+#
+logid = [
+    {"aeg": "2025-03-10", "tüüp": "Info", "sõnum": "Server käivitati"},
+    {"aeg": "2025-03-10", "tüüp": "Hoiatus", "sõnum": "Mälu kasutus ületas 80% piiri"},
+    {"aeg": "2025-03-10", "tüüp": "Viga", "sõnum": "Andmebaasi ühendus nurjus"},
+    {"aeg": "2025-03-10", "tüüp": "Info", "sõnum": "Kasutaja 'admin' sisselogimine õnnestus"},
+    {"aeg": "2025-03-10", "tüüp": "Viga", "sõnum": "Faili kirjutamine ebaõnnestus"},
+    {"aeg": "2025-03-10", "tüüp": "Hoiatus", "sõnum": "Ühendus andmebaasiga katkestatud"},
+    {"aeg": "2025-03-10", "tüüp": "Info", "sõnum": "Kohandatud seadistused rakendatud"},
+    {"aeg": "2025-03-10", "tüüp": "Viga", "sõnum": "HTTP päring ebaõnnestus: 404 Not Found"}
+]
+infoCount = 0
+
+for elem in logid:
+        print(log[elem]["tüpp"])):
+elif elem in logid:
+    if elem["sõnum"] == "Server käivitati":
+        print(log)
+print(infoCount)
