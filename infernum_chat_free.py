a2590089d0da682bffdf145b2b23dcdd83d6020e
NUNGEN <nikita.voronin0211@gmail.com>
2025-03-27 15:42:13 +0200

    Add files via upload

---
+++ infernum_chat_free.py
@@ -0,0 +1,104 @@
+# class NumberAnalizer:
+#     def __init__(self,number,IsEven,IsPositive,Summury):
+#         self.done = done
+#         self.number == input("")
+#     #    super.
+#     def IsEven:
+#         self.number = number
+#         if number % 2 == 0:
+#             print(f"This is an even number, {number}.")
+#         else:
+#         self.__number == False
+#             print(f"This is't an even number, {number}.")
+# 
+#     def IsPositive:
+#         self.__positive_num == positive
+#             else:
+#         self.
+#     def Summury(self):
+#     print(self,"number","IsEven","IsPositive","Summury")
+#     
+#     
+# number.NumberAnalizer(4)
+# number.IsEven()
+# number.IsPossitive()
+# number.Summury()
+
+
+# class NumAnalizer:
+#     def __init__(self,number):
+#         self.__number = number
+#         self.NumIsPositive = False
+#         self.NumIsEven = False
+#     def IsPositive(self)
+#         if self.__number >= 0:
+#             print("Number is positive")
+#             self.NumIsPositive = True
+#         else:
+#             print("nUMBER IS NEGATIve")
+
+
+class Person:
+    def __init__(self,name,personal_code):
+        self.name = name
+        self.personal_code = personal_code
+        self.accounts = []
+        
+    def add_accounts(self):
+        print(self,accounts)
+        
+    def get_accounts(self):
+        print(self.accounts)   
+    def get_info(self):
+        print(f"personal_code:{self.__personal_code}")
+        print(f"name:{self.__name}")
+        
+
+class BankAccount:
+    def __init__(self,Person)
+        self.__account_number = account_number
+        self.__balance = balance
+        
+    def add(self,amount):
+        self.__balance += amount
+        
+    def withdraw(self,amount):
+        if amount > self.__balance:
+        print:("Not enough money")
+    else:
+        self.balance -= amount
+        print("Money sent")
+    def get_balance(self): return self.__balance
+    def get_account_number(self): return self.__account_number
+    
+class Client(Person):
+    def __init__(self,name,personal_code,email,phone):
+        super().__init__(name, personal_code)
+        
+    def email:
+        self.userEmail = userEmail
+        
+    def clientType(regular,premium,business):
+        self.__regularC = regularC
+        self.__premiumCL = premiumCL
+        self.__businessCL = businessC
+        
+    def account_summary(self):
+        if self.__balance <= 1000:
+            self.clientType = "Regular"
+        elif self.__balance > 10000: self.clientType = "premium"
+        else:
+            self.clientType = "business"
+        
+        return:
+            print(f"name:{self.__name}")
+            print(f"email:{self.email}")
+            print(f"phone")
+            print(f"")
+            
+        
+    
+person = Person("Fedja",229)
+person.add_account
+
+        
