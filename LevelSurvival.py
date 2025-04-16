ec89bb3a225a200947d45ae3460de0422ea93add
NUNGEN <nikita.voronin0211@gmail.com>
2025-04-16 11:41:58 +0300

    Add files via upload

---
+++ LevelSurvival.py
@@ -0,0 +1,52 @@
+import random
+
+class Level:
+    def __init__(self, player_on_each_level, player_amount):
+        self.player_on_each_level == player_amount
+        self.player_amount == 2
+        levels = (range("level_1", "level_5"))
+["level_1", "level_2", "level_3", "level_4", "level_5",]
+    def (self, player_on_each_level, death, player_amount, minimum_ammount_of_recources)
+        self.minimum_ammount_of_recources = (8)
+        if minimum_ammount_of_recources < 8
+            print("Level is gone")
+        else:
+            print("Still alive")
+   
+class platform(self, level, resourse_ammount, method_of_obtainment, method_of_resource_transfer):           
+    def __init__(self):
+            self.resourse_ammount = ["100"]
+            self.method_of_obtainment = method_of_obtainment
+            self.method_of_resource_transfer = method_of_resource_transfer
+            self.exact_amount = exact_amount
+            self.level_change = level_change
+        for level_change in range(levels):
+            print(f"Current level {level_change}")
+        
+    def ResourceChange(self,):
+        if resource_ammount <= (100 * 0,32):
+            print(f"{resource_ammount} Change is done")
+        else:
+            print("Resources are hold")
+#       self.
+#   print("-------Level has changed--------")
+    
+    
+class Playerok(self,level):
+    def __init__(self, players, player_choice, death):
+        self.players = [player_1, player_2, player_3, player_4, player_5, player_6, player_7, player_8, player_9, player_10]
+        self.player_choice = player_choice
+        self.player_choice_is_to_transfer = player_choice_is_to_transfer
+        self.player_choice_is_to_hold = player_choice_is_to_hold
+        self.death = death(minimum_ammount_of_recources)
+        self.life = life(minimum_ammount_of_recources)
+    def choice(self, platform, resourse_ammount, player_choice)
+        if randint(player_choice_is_to_transfer,player_choice_is_to_transfer) is True:
+            print(f"Player choose to {player_choice_is_to_transfer}, {player_choice_is_to_transfer}")
+        else:
+            print(f"the consiquence is {death} , {live}")
+            
+
+    
+#    def player_transfer()
+        
